c54c24a3717b3ca0b02fe84631c3bbe3fc865449
Miqueias <miqueias.rihs@gmail.com>
2022-03-02 20:04:38 -0300

    seila

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from http import client
 from urllib import response
 
 from django.urls import resolve
@@ -12,6 +13,13 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
 
     def test_home_page_returns_corret_html(self):
         response = self.client.get('/')
